540ae07fe80fd12a24498427b80dfd1509e836be
Anwesh43 <anweshthecool0@gmail.com>
2020-12-29 01:40:51 +0530

    adding constants and libraries

--- piemultilinerotview/src/main/java/com/example/piemultilinerotview/MultiLinePieRotView.kt
+++ piemultilinerotview/src/main/java/com/example/piemultilinerotview/MultiLinePieRotView.kt
@@ -1,2 +1,29 @@
 package com.example.piemultilinerotview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#4CAF50",
+    "#2196F3",
+    "#795548",
+    "#3F51B5"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 2.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val pies : Int = 6
+val rFactor : Float = 8.9f
