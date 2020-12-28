ebb65ede80586bc137cdcb77b9c24ba182d1001c
Anwesh43 <anweshthecool0@gmail.com>
2020-12-29 01:42:10 +0530

    adding Int and Float extension methods

--- piemultilinerotview/src/main/java/com/example/piemultilinerotview/MultiLinePieRotView.kt
+++ piemultilinerotview/src/main/java/com/example/piemultilinerotview/MultiLinePieRotView.kt
@@ -27,3 +27,8 @@ val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
 val pies : Int = 6
 val rFactor : Float = 8.9f
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
